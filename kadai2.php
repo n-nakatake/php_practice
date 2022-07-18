d11113b3ef24e8e4fae115e4e25472ad0bca9217
n-nakatake <nakatake@iroiro-inc.com>
2022-07-18 01:02:38 +0000

    first commit

---
+++ kadai2.php
@@ -0,0 +1,35 @@
+<?PHP
+
+//1
+$name = "Naoko";
+if ( $name == "Naoko" ) {
+    echo "私は" .$name ."です";
+} else {
+    echo $naem ."ではありません";
+}
+echo "\n";
+
+//2
+$total = 0;
+for ($i = 0; $i <= 10000; $i++){
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+//3
+$fruits = array("peach","muscat","kiwifruit","strawberry","pineapple");
+foreach($fruits  as $index => $fruit){
+    echo $index+1 ."つ目の要素は" . $fruit;
+    echo "\n";
+}
+
+//4
+$start =1;
+$end =100;
+for( $i = $start; $i <= $end; $i++){
+    if( $i % 5 == 0 ){
+        echo $i;
+        echo "\n";
+    }
+}
