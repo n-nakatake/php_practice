043ef0cd22db6963624a8dbd706048831acbbd2c
n-nakatake <nakatake@iroiro-inc.com>
2022-07-19 13:34:56 +0000

    second commit

--- kadai.php
+++ kadai.php
@@ -8,7 +8,7 @@ echo PHP_EOL;
 
 // 2
 echo "２．";
-$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月", ];
 echo $array_month[7];
 echo PHP_EOL;
 
@@ -16,8 +16,8 @@ echo PHP_EOL;
 echo "３．";
 $hello ="Hello,";
 $name = "Naoko";
-$word = "s Word!";
-echo $hello .$name .$word;
+$word = "'s Word!";
+echo $hello .$name . $word;
 echo PHP_EOL;
 
 //4
@@ -41,7 +41,7 @@ $calendar_2018 = [
   "September" => "9月",
   "October" => "10月",
   "November" => "11月",
-  "December" => "12月"
+  "December" => "12月",
 ];
 
 // 12月を表示する
